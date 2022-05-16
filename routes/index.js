435e6c2c19c9bbe26062796f30a9127cbf683058
huaphucdung <huaphucdung2001@gmail.com>
2022-05-16 19:27:35 +0700

    front-end login và register

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  content = {
+    title: 'Login',
+    css: 'login.css'
+  }
+  res.render('login', content);
+});
+
+router.get('/register', function(req, res, next) {
+
+  content = {
+    title: 'Register',
+    css: 'login.css'
+  }
+  res.render('register', content);
 });
 
 module.exports = router;
