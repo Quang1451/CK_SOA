cd6c1e967492a5d72273a2268af523f78c182ef9
huaphucdung <huaphucdung2001@gmail.com>
2022-05-17 10:13:56 +0700

    remove wallet.js

--- models/wallet.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const walletSchema = mongoose.Schema({
-    userId: String,
-    money: Number
-})
-
-const wallet = mongoose.model('Wallet', walletSchema)
-module.exports = wallet
